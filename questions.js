9e63f8b7c56bab446b80d7339e3a978c39a341e3
Nozonn <125192809+Nozonn@users.noreply.github.com>
2024-11-01 23:19:01 +0100

    Question 2 - 3 - 4
    Have 3 questions
    Idx 1 -> 3

--- questions.js
+++ questions.js
@@ -6,8 +6,23 @@ window.data = [
     },
 
     {
-        question: "Test",
-        response: "1",
-        wrong_responses: ["2", "3", "4"]
+        question: "Facile : Jean de La Fontaine fait parti de quel mouvement littéraire ?",
+        response: "Classicisme",
+        wrong_responses: ["Romantisme", "Humanisme", "Réalisme"]
+    },
+
+    {
+        question: "Moyen : Quels sont les activités de Jean de La Fontaine ?",
+        response: "Poète, moraliste, dramaturge, librettiste, romancier et fabuliste",
+        wrong_responses: ["Poète, fabuliste", "moraliste, fabuliste, Poète", 
+            "dramaturge, librettiste, fabuliste, moraliste"]
+    }, 
+
+    { 
+        question: "Difficile : Quels sont les dates de naissances et de mort de La Fontaine ?",
+        response: "8 juillet 1621 - 13 avril 1695",
+        wrong_responses: ["7 juillet 1622 - 13 avril 1695", "7 juillet 1620 - 13 avril 1695", 
+            "7 juillet 1621 - 14 avril 1695"]
+
     }
 ]
