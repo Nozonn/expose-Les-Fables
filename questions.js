e21819abb76d952a9beb86bc72eee3ebfafbc158
Nozonn <125192809+Nozonn@users.noreply.github.com>
2024-11-03 00:33:14 +0100

    Upload all html files and js  and adding new quiz
    Improving the reading of the files
    Improving the styles of the sites
    Adding  new question

--- questions.js
+++ questions.js
@@ -1,16 +1,19 @@
 window.data = [
+    // question1.html
     {
         question: "Mais à quoi cela sert d'écrire les fables ?",
         response: "Plaire et Instruire",
         wrong_responses: ["Ennuyer et Instruire", "Être long à lire et Ennuyer", "Plaire et Rendre moins intelligent"]
     },
 
+    // question2-3-4.html
     {
         question: "Facile : Jean de La Fontaine fait parti de quel mouvement littéraire ?",
         response: "Classicisme",
         wrong_responses: ["Romantisme", "Humanisme", "Réalisme"]
     },
 
+    // question2-3-4.html
     {
         question: "Moyen : Quels sont les activités de Jean de La Fontaine ?",
         response: "Poète, moraliste, dramaturge, librettiste, romancier et fabuliste",
@@ -18,11 +21,20 @@ window.data = [
             "dramaturge, librettiste, fabuliste, moraliste"]
     }, 
 
+    // question2-3-4.html
     { 
         question: "Difficile : Quels sont les dates de naissances et de mort de La Fontaine ?",
         response: "8 juillet 1621 - 13 avril 1695",
         wrong_responses: ["7 juillet 1622 - 13 avril 1695", "7 juillet 1620 - 13 avril 1695", 
             "7 juillet 1621 - 14 avril 1695"]
 
-    }
-]
+    },
+
+    // question-qu-est-ce-que-c-est-une-fable.html
+    {
+        question: "Qu'est-ce qu'une fable ?",
+        response: "Un court récit en vers ou en prose",
+        wrong_responses: ["Un long récit en vers", 
+            "Un court récit uniquement en vers", "Un long récit uniquement en prose"]
+    },
+];
--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-function setEventChoose() {
-    for (let i=0; i<choices.length; i++) {
-        choices[i].disabled = false;
-        choices[i].addEventListener("click", ev=> {
-            choose(i+1);
-        });
-    };
-};
+// Function to generate a new quiz question
+function quizSpawner() {
+    document.getElementById("questionContainer").innerHTML = `<p id="question">${dataSel.question}</p>`
 
+    responses = [dataSel.response, ...dataSel.wrong_responses];
+    idxUnavailable = [];
+
+    if (btns.length > 0) {
+        btns.forEach(btn => {
+            btn.remove();
+        });
+    }
+
+    for (let i=0; i<4; i++) {
+        const btn = document.createElement("button");
+        btn.id = `choice${i+1}`;
+        btn.classList.add("choice");
+        btn.name = `${i+1}`;
+        let idx = Math.floor(Math.random()*responses.length);
+
+        while ( idxUnavailable.includes(idx) ) {
+            idx = Math.floor(Math.random()*responses.length);
+        }
+
+        idxUnavailable.push(idx)
+        btn.innerText = responses[idx]
+
+        if ( responses[idx] === responses[0] ) {
+            btn.classList.add("good-response")
+        }
+        
+        btns.push(btn);
+        document.getElementById("quiz").appendChild(btn);
+    };
+}
+
+// Funtions to manage the style of the buttons 
 function choose(btnName) {
     for (let i=0; i<choices.length; i++) {;
         choices[i].setAttribute("style", "background: var(--fg);color: var(--bg);")
@@ -17,30 +45,137 @@ function choose(btnName) {
     choices.namedItem(btnName).classList.add("select");
 };
 
+function setEventChoose() {
+    for (let i=0; i<choices.length; i++) {
+        choices[i].disabled = false;
+        choices[i].addEventListener("click", ev=> {
+            choose(i+1);
+        });
+    };
+};
+
 function delEventChoose() {
     for (let i=0; i<choices.length; i++) {
         choices[i].disabled = true
     };
 };
+// =============================================
 
+// Fuctions to validate the answer and do the animations if the answer is correct or wrong
+function fwStart() {
+    myFW1.start();myFW2.start();myFW3.start();myFW4.start();
+
+    let boxAnimation = document.createElement("div");
+    boxAnimation.id = "boxAnimation";
+    document.getElementsByTagName("body")[0].appendChild(boxAnimation)
+
+    const audio = document.createElement("audio");
+    audio.src = "fireworks.mp3";
+    audio.autoplay = true;
+    boxAnimation.appendChild(audio);
+
+    return false;
+};
+
+function wrongAnimation() {
+    let boxAnimation = document.createElement("div");
+    boxAnimation.id = "boxAnimation";
+    document.getElementsByTagName("body")[0].appendChild(boxAnimation)
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "cacaEmoji.jpg");
+    img.id = "imgAnimationWrongAnswer"
+    img.style = `
+    width: 100px; height: 100px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(4) rotate(360deg);
+    `;
+    const audio = document.createElement("audio");
+    audio.src = "wrong-answer.mp3";
+    audio.autoplay = true;
+
+    boxAnimation.appendChild(img)
+    boxAnimation.appendChild(audio);
+    
+}
+
+function funcValidation() {
+    for (btn of btns) {
+        if (btn.innerText === dataSel.response) {
+            const lClass = btn.classList;
+            for (c of lClass) {
+                if (c === "select") {
+                    return fwStart();
+                };
+            }; 
+            return wrongAnimation();
+            
+        };
+    };
+};
+// ==============================================
+
+// Function to go to the next question
+function funcNext() {
+    document.getElementById("boxAnimation").remove();
+
+    for (let i=0; i<choices.length; i++) {;
+        choices[i].setAttribute("style", "background: var(--fg);color: var(--bg);")
+        choices[i].classList.remove("select")
+    };
+
+    myFW1.stop();myFW2.stop();myFW3.stop();myFW4.stop();
+
+    idxDataSel ++;
+    if (idxDataSel < firstQuiz+nbQuiz) {
+        dataSel = data[idxDataSel];
+        quizSpawner();
+    }
+    
+    return false;
+}
+
+
+// Creation of the quiz
+let responses = [];
+let idxUnavailable = [];
+const btns = []; // List of buttons
+quizSpawner();
+
+// Elements of structure
 const quizForm = document.getElementById("quiz");
 const btnConfimation = document.getElementById("validation");
 const btnNext = document.getElementById("next");
-const choices = quizForm.children
+const choices = quizForm.children; // HTML Collection of buttons
 
+// Display
+setEventChoose(); // Style of the selected button
+btnNext.disabled = true; // Enable next button
+
+// Event listeners
 quizForm.addEventListener("submit", ev=> {
-    ev.preventDefault();
+    ev.preventDefault(); // Prevent form submission
 });
 
-
-setEventChoose();
-
 btnConfimation.addEventListener("click", ev=>{
+    funcValidation();
     delEventChoose();
+
+    btnConfimation.disabled = true;
+    btnNext.disabled = false;
+
 });
 
 btnNext.addEventListener("click", ev=> {
-    for (let i=0; i<choices.length; i++) {
-        choices[i].disabled = false;
+    for (let i=0; i<btns.length; i++) {
+        btns[i].disabled = false;
     };
+
+    funcNext();
+    setEventChoose();
+
+    btnConfimation.disabled = false;
+    btnNext.disabled = true;
 });
