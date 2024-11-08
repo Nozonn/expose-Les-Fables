e209194de8070684d424d2d7a4701a0e25bee600
Nozonn <125192809+Nozonn@users.noreply.github.com>
2024-11-08 21:47:25 +0100

    Question video modification

--- questions.js
+++ questions.js
@@ -4,7 +4,7 @@ window.data = [
         question: "Mais à quoi cela sert d'écrire les fables ?",
         response: "Plaire et Instruire",
         wrong_responses: ["Ennuyer et Instruire", "Être long à lire et Ennuyer", "Plaire et Rendre moins intelligent"],
-        wrongAnswerAnimation: "cat-nop.mp4",
+        wrongAnswerAnimation: "./cat-nop.mp4",
     },
 
     // question2-3-4.html
@@ -12,7 +12,7 @@ window.data = [
         question: "Facile : Jean de La Fontaine fait parti de quel mouvement littéraire ?",
         response: "Classicisme",
         wrong_responses: ["Romantisme", "Humanisme", "Réalisme"],
-        wrongAnswerAnimation: "zidane-honon.mp4",
+        wrongAnswerAnimation: "./zidane-honon.mp4",
     },
 
     // question2-3-4.html
@@ -21,7 +21,7 @@ window.data = [
         response: "Poète, moraliste, dramaturge, librettiste, romancier et fabuliste",
         wrong_responses: ["Poète, fabuliste", "moraliste, fabuliste, Poète", 
             "dramaturge, librettiste, fabuliste, moraliste"],
-        wrongAnswerAnimation: "asterix-non.mp4",
+        wrongAnswerAnimation: "./asterix-non.mp4",
     }, 
 
     // question2-3-4.html
@@ -30,7 +30,7 @@ window.data = [
         response: "8 juillet 1621 - 13 avril 1695",
         wrong_responses: ["7 juillet 1622 - 13 avril 1695", "7 juillet 1620 - 13 avril 1695", 
             "7 juillet 1621 - 14 avril 1695"],
-        wrongAnswerAnimation: "mario-no.mp4"
+        wrongAnswerAnimation: "./mario-no.mp4"
 
     },
 
@@ -40,6 +40,6 @@ window.data = [
         response: "Un court récit en vers ou en prose",
         wrong_responses: ["Un long récit en vers", 
             "Un court récit uniquement en vers", "Un long récit uniquement en prose"],
-        wrongAnswerAnimation: "persoBizarre-nop.mp4",
+        wrongAnswerAnimation: "./persoBizarre-nop.mp4",
     },
 ];
